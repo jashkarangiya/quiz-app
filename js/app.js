eda2466f2183ebfa09ddff05c04dad43d1768c96
jashkarangiya <jashkarangiya@gmail.com>
2023-02-04 22:18:46 +0530

    Updated some files

---
+++ js/app.js
@@ -0,0 +1,41 @@
+const question = [{
+        'que': "Which of the following is not a character of One Piece?",
+        'a': "Vinsmoke Sanji",
+        "b": "Nami",
+        "c": "Levi Ackerman",
+        "d": "Xebec D shanks.",
+        'correct': "c"
+    },
+    {
+        'que': "Who was Naruto love?",
+        'a': "Sasuke Uchicha",
+        "b": "Sakura Haruno",
+        "c": "Hinata Hyuga",
+        "d": "TenTen",
+        'correct': "c"
+    },
+    {
+        'que': "Who breaks Wall Maria?",
+        'a': "Eren Yeager",
+        "b": "Reiner Braun",
+        "c": "Zeke Yeager",
+        "d": "Levi Ackerman",
+        'correct': "b"
+    },
+    {
+        'que': "What color was Tanjiro's Sword?",
+        'a': "Black",
+        "b": "Pink",
+        "c": "Yellow",
+        "d": "Red",
+        'correct': "a"
+    },
+    {
+        'que': "What color was Tanjiro's Sword?",
+        'a': "Black",
+        "b": "Pink",
+        "c": "Yellow",
+        "d": "Red",
+        'correct': "a"
+    },
+]
